dd1212346c1138d6ed4c3adce6ec5d41876350ce
Moz <amazumda@purdue.edu>
2025-01-24 18:17:21 -0500

    Continue Ch7 - texture memory

--- learn1/learn1/heattransfer_gpu.cuh
+++ learn1/learn1/heattransfer_gpu.cuh
@@ -8,6 +8,8 @@
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 
+#define DIM 1000
+#define SPEED 5
 
 // Definitions to bypass red squiggly lines when doing kernel calls
 // https://forums.developer.nvidia.com/t/intellisense-error-in-brand-new-cuda-project-in-vs2019/111921/7
@@ -28,4 +30,61 @@
 __global__ void heattransfer_gpu()
 {
 
+}
+
+/// STEP 1
+// Given the grid of input (starter) temps, copy temp. of cells into the input grid of the main kernel
+__global__ void copy_const_kernel(float* iptr, const float* cptr)
+{	
+
+	// Do mapping from threadIdx or BlockIdx to x and y-coordinate of the window
+	int x = threadIdx.x + blockIdx.x * blockDim.x;
+	int y = threadIdx.y + blockIdx.y * blockDim.y;
+	// Linear offset into our buffers
+	int offset = x + y * blockDim.x * gridDim.x;
+
+	// Copy temp. from cptr - constatnt to iptr - input
+	// Copy is performed only if cell has non-zero value in const. grid
+	// This is done to preserve temps for any non-zero temps in the input for each step
+	if (cptr[offset] != 0)
+	{
+		iptr[offset] = cptr[offset];
+	}
+
+}
+
+/// STEP 2
+// Each thread takes up responsibility for 1 cell of our grid
+// Thread reads temps of that cell, temps of surrounding cells, computes updates, and updates the cell's temp. with a new value
+__global__ void blend_kernel(float* outSrc, const float* inSrc)
+{
+	// Do mapping from threadIdx or BlockIdx to x and y-coordinate of the window
+	int x = threadIdx.x + blockIdx.x * blockDim.x;
+	int y = threadIdx.y + blockIdx.y * blockDim.y;
+	// Linear offset into our buffers
+	int offset = x + y * blockDim.x * gridDim.x;
+
+	// Calculating the positions of the neighbors, based on offset value since we use offset value as index
+	int left = offset - 1;
+	int right = offset + 1;
+	// Now here, we need to adjust for border indexes so that cells on the edges do not wrap around
+	// Accounting for first value of x (first row of grid)
+	if (x == 0)
+		left++;
+	// Accounting for last value of x (last row of grid)
+	if (x == DIM - 1)
+		right++;
+
+	int top = offset - DIM;
+	int bottom = offset + DIM;
+	// Accounting for first value of y (first column of grid)
+	if (y == 0)
+		top += DIM;
+	// Accounting for last value of y (last column of grid)
+	if (y == DIM - 1)
+		bottom -= DIM;
+
+	outSrc[offset] = inSrc[offset] + SPEED * (
+		inSrc[top] + inSrc[bottom] + inSrc[left] + inSrc[right]
+		- inSrc[offset] * 4);
 }
